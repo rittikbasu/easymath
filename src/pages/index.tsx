947294019b09f66923aa51f1a730dd5ec3365ae4
rittikbasu <irittik@gmail.com>
2023-07-04 14:36:21 +0530

    feat: add total + footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import Link from "next/link";
+
 import { Product } from "../components/Product";
+import { Table } from "../components/Table";
 
 export default function App() {
-  const [pr1Price, setPr1Price] = useState(0);
-  const [pr2Price, setPr2Price] = useState(0);
-  const [pr3Price, setPr3Price] = useState(0);
+  const [pr1Price, setPr1Price] = useState(18);
+  const [pr2Price, setPr2Price] = useState(17);
+  const [pr3Price, setPr3Price] = useState(20);
   const [pr4Price, setPr4Price] = useState(0);
   const [pr5Price, setPr5Price] = useState(0);
 
@@ -14,44 +17,36 @@ export default function App() {
   const [pr4Quantity, setPr4Quantity] = useState(0);
   const [pr5Quantity, setPr5Quantity] = useState(0);
   return (
-    <>
-      {/* title: Easy Math */}
+    <div className="max-w-md md:mx-auto flex flex-col h-screen antialiased overflow-auto">
       <h1 className="text-3xl text-center mt-8 font-mono">Easy Math ✨</h1>
-      <div className="flex justify-center gap-x-6 mt-8 border-2 border-zinc-700 py-2 rounded-2xl mx-2">
+      <div className="flex justify-center gap-x-6 md:mt-14 mt-8 border border-zinc-700 py-2 rounded-2xl mx-2">
         <Product
-          name="Double Mint"
+          name="Double Switch"
           color="bg-blue-700"
-          defaultPrice={18}
           price={pr1Price}
           quantity={pr1Quantity}
           setQuantity={setPr1Quantity}
-          step={1}
           setPrice={setPr1Price}
         ></Product>
         <Product
           name="Marlboro Light"
           color="bg-orange-600"
-          defaultPrice={20}
           price={pr2Price}
           quantity={pr2Quantity}
           setQuantity={setPr2Quantity}
-          step={1}
           setPrice={setPr2Price}
         ></Product>
         <Product
-          name="Other Cig"
+          name="Marlboro Advance"
           color="bg-red-600"
-          defaultPrice={18}
           price={pr3Price}
           quantity={pr3Quantity}
           setQuantity={setPr3Quantity}
-          step={1}
           setPrice={setPr3Price}
         ></Product>
         <Product
           name="Cold Drink"
           color="bg-yellow-950"
-          defaultPrice={0}
           price={pr4Price}
           quantity={pr4Quantity}
           setQuantity={setPr4Quantity}
@@ -62,7 +57,6 @@ export default function App() {
         <Product
           name="Chips/ Others"
           color="bg-lime-400"
-          defaultPrice={0}
           price={pr5Price}
           quantity={pr5Quantity}
           setQuantity={setPr5Quantity}
@@ -71,82 +65,55 @@ export default function App() {
         ></Product>
       </div>
 
-      <div className="shadow-md mt-12 border-2 border-zinc-700 py-2 rounded-2xl mx-2">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Quantity
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Double Mint
-              </th>
-              <td className="px-6 py-4 text-center">{pr1Quantity}</td>
-              <td className="px-6 py-4">₹{pr1Price}</td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Marlboro Light
-              </th>
-              <td className="px-6 py-4 text-center">{pr2Quantity}</td>
-              <td className="px-6 py-4">₹{pr2Price}</td>
-            </tr>
-            <tr className="bg-white border-b dark:border-gray-700 dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Other Cig
-              </th>
-              <td className="px-6 py-4 text-center">{pr3Quantity}</td>
-              <td className="px-6 py-4">₹{pr3Price}</td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Cold Drink
-              </th>
-              <td className="px-6 py-4 text-center">-</td>
-              <td className="px-6 py-4">₹{pr4Price}</td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Chips/ Others
-              </th>
-              <td className="px-6 py-4 text-center">-</td>
-              <td className="px-6 py-4">₹{pr5Price}</td>
-            </tr>
-            <tr className="text-xs text-gray-700 uppercase dark:text-gray-400">
-              <th scope="row" className="px-6 py-3">
-                Total
-              </th>
-              <td className="px-6 py-3"></td>
-              <td className="px-6 py-3 font-bold">₹2999</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="md:mt-32 mt-12 border border-zinc-700 pt-2 rounded-2xl mx-2 overflow-hidden">
+        <Table
+          pr1Price={pr1Price}
+          pr2Price={pr2Price}
+          pr3Price={pr3Price}
+          pr5Price={pr5Price}
+          pr1Quantity={pr1Quantity}
+          pr2Quantity={pr2Quantity}
+          pr3Quantity={pr3Quantity}
+          pr4Quantity={pr4Quantity}
+          pr5Quantity={pr5Quantity}
+          setPr1Price={setPr1Price}
+          setPr2Price={setPr2Price}
+          setPr3Price={setPr3Price}
+          setPr4Price={setPr4Price}
+          setPr5Price={setPr5Price}
+          setPr1Quantity={setPr1Quantity}
+          setPr2Quantity={setPr2Quantity}
+          setPr3Quantity={setPr3Quantity}
+          setPr4Quantity={setPr4Quantity}
+          setPr5Quantity={setPr5Quantity}
+        ></Table>
       </div>
-    </>
+
+      {/* Show total */}
+      <div className="flex justify-center items-center md:mt-24 mt-12">
+        <div className="flex flex-col justify-center items-center space-y-2">
+          <p className="text-2xl font-bold text-gray-800 dark:text-white">
+            Total
+          </p>
+          <p className="text-4xl font-bold text-gray-800 dark:text-blue-500">
+            ₹
+            {pr1Price * pr1Quantity +
+              pr2Price * pr2Quantity +
+              pr3Price * pr3Quantity +
+              pr4Quantity +
+              pr5Price}
+          </p>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer className="mt-auto px-5 py-4 text-center bg-black">
+        <p className="font-mono">
+          <Link href="https://rittik.io" className="hover:text-indigo-500">
+            Made with ❤️ by Rittik
+          </Link>
+        </p>
+      </footer>
+    </div>
   );
 }
