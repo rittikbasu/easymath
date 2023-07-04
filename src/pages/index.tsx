7388df481b3098f1b4cd75f83bb082a9419ce85f
rittikbasu <irittik@gmail.com>
2023-07-04 10:34:47 +0530

    feat: product component + table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,152 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
+import { Product } from "../components/Product";
 
-const inter = Inter({ subsets: ['latin'] })
+export default function App() {
+  const [pr1Price, setPr1Price] = useState(0);
+  const [pr2Price, setPr2Price] = useState(0);
+  const [pr3Price, setPr3Price] = useState(0);
+  const [pr4Price, setPr4Price] = useState(0);
+  const [pr5Price, setPr5Price] = useState(0);
 
-export default function Home() {
+  const [pr1Quantity, setPr1Quantity] = useState(0);
+  const [pr2Quantity, setPr2Quantity] = useState(0);
+  const [pr3Quantity, setPr3Quantity] = useState(0);
+  const [pr4Quantity, setPr4Quantity] = useState(0);
+  const [pr5Quantity, setPr5Quantity] = useState(0);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      {/* title: Easy Math */}
+      <h1 className="text-3xl text-center mt-8 font-mono">Easy Math ✨</h1>
+      <div className="flex justify-center gap-x-6 mt-8 border-2 border-zinc-700 py-2 rounded-2xl mx-2">
+        <Product
+          name="Double Mint"
+          color="bg-blue-700"
+          defaultPrice={18}
+          price={pr1Price}
+          quantity={pr1Quantity}
+          setQuantity={setPr1Quantity}
+          step={1}
+          setPrice={setPr1Price}
+        ></Product>
+        <Product
+          name="Marlboro Light"
+          color="bg-orange-600"
+          defaultPrice={20}
+          price={pr2Price}
+          quantity={pr2Quantity}
+          setQuantity={setPr2Quantity}
+          step={1}
+          setPrice={setPr2Price}
+        ></Product>
+        <Product
+          name="Other Cig"
+          color="bg-red-600"
+          defaultPrice={18}
+          price={pr3Price}
+          quantity={pr3Quantity}
+          setQuantity={setPr3Quantity}
+          step={1}
+          setPrice={setPr3Price}
+        ></Product>
+        <Product
+          name="Cold Drink"
+          color="bg-yellow-950"
+          defaultPrice={0}
+          price={pr4Price}
+          quantity={pr4Quantity}
+          setQuantity={setPr4Quantity}
+          step={5}
+          setPrice={setPr4Price}
+          hidePrice={true}
+        ></Product>
+        <Product
+          name="Chips/ Others"
+          color="bg-lime-400"
+          defaultPrice={0}
+          price={pr5Price}
+          quantity={pr5Quantity}
+          setQuantity={setPr5Quantity}
+          setPrice={setPr5Price}
+          hideQuantity={true}
+        ></Product>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="shadow-md mt-12 border-2 border-zinc-700 py-2 rounded-2xl mx-2">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Quantity
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Double Mint
+              </th>
+              <td className="px-6 py-4 text-center">{pr1Quantity}</td>
+              <td className="px-6 py-4">₹{pr1Price}</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Marlboro Light
+              </th>
+              <td className="px-6 py-4 text-center">{pr2Quantity}</td>
+              <td className="px-6 py-4">₹{pr2Price}</td>
+            </tr>
+            <tr className="bg-white border-b dark:border-gray-700 dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Other Cig
+              </th>
+              <td className="px-6 py-4 text-center">{pr3Quantity}</td>
+              <td className="px-6 py-4">₹{pr3Price}</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Cold Drink
+              </th>
+              <td className="px-6 py-4 text-center">-</td>
+              <td className="px-6 py-4">₹{pr4Price}</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Chips/ Others
+              </th>
+              <td className="px-6 py-4 text-center">-</td>
+              <td className="px-6 py-4">₹{pr5Price}</td>
+            </tr>
+            <tr className="text-xs text-gray-700 uppercase dark:text-gray-400">
+              <th scope="row" className="px-6 py-3">
+                Total
+              </th>
+              <td className="px-6 py-3"></td>
+              <td className="px-6 py-3 font-bold">₹2999</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
